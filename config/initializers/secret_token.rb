9391f88c8695ed21628589104147a2418f329da8
Ruth Foster <rshiroma@dhcp-168-105-81-26.wireless.manoa.hawaii.edu>
2014-01-05 17:34:32 -1000

    my second awesome rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Embedder::Application.config.secret_key_base = '915b37ad4937761ecb72fcfd258dfb3148318ba64c1422a146d760e5b34de2ebdc36cb27e8a1f7ab870626e97f25e7db1f4833021cd14f3176354924ba583ffd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Embedder::Application.config.session_store :cookie_store, key: '_embedder_session'
